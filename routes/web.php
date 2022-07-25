e235bfabb187e8df1f5ddd5ed71d9ccfab611f49
saito ryosuke <ryosuke5230@gmail.com>
2022-07-25 10:54:34 +0000

    課題０４

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-});
+Route::get('admin/news/create', 'Admin\NewsController@add');
+Route::get('://XXXXXX.jp/XXX', 'Admin\AAAController@bbb');
+Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/edit','Admin\ProfileController @edit');
