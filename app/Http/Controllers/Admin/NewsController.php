4a02fbe8d8d97832d5308cf91fdc10e28eee864b
saito ryosuke <ryosuke5230@gmail.com>
2022-08-21 00:15:14 +0000

    serve

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -14,5 +14,14 @@ class NewsController extends Controller
 view('admin.news.create');
     }
     
+
+
+ // 以下を追記
+  public function create(Request $request)
+  {
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+  }  
 }
+
 
