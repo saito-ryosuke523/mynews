543fa9e0af849fb2a80a01f6b00521404dc9e271
saito ryosuke <ryosuke5230@gmail.com>
2022-08-27 06:40:25 +0000

    修正

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,16 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 
     //
     Route::get('profile/create','Admin\ProfileController@add');
     Route::post('profile/create','Admin\ProfileController@create');
-    Route::post('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@create');
+    
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
 });
 // Route::get('://XXXXXX.jp/XXX', 'Admin\AAAController@bbb');
 
