986c30dad472d7ba35dc3d6234b37e3cbdb92269
saito ryosuke <ryosuke5230@gmail.com>
2022-09-02 00:21:57 +0000

    　課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,19 +14,16 @@ class NewsController extends Controller
 view('admin.news.create');
     }
     
-
-
- // 以下を追記
-  public function create(Request $request)
+public function create(Request $request)
   {
-      // 以下を追記
-      // Varidationを行う
+
+      // Varidationをおこなう
       $this->validate($request, News::$rules);
 
-      $news = new News;
+      $news = new News();
       $form = $request->all();
 
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      // formに画像があれば、保存する
       if (isset($form['image'])) {
         $path = $request->file('image')->store('public/image');
         $news->image_path = basename($path);
@@ -33,32 +31,70 @@ view('admin.news.create');
           $news->image_path = null;
       }
 
-      // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
       unset($form['image']);
-
       // データベースに保存する
       $news->fill($form);
       $news->save();
 
       return redirect('admin/news/create');
   }
-}
 
-// 以下を追記
   public function index(Request $request)
   {
       $cond_title = $request->cond_title;
       if ($cond_title != '') {
-          // 検索されたら検索結果を取得する
           $posts = News::where('title', $cond_title)->get();
       } else {
-          // それ以外はすべてのニュースを取得する
           $posts = News::all();
       }
       return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
   }
 
-}
+  // 以下を追記
 
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      if (empty($news)) {
+        abort(404);    
+      }
+      return view('admin.news.edit', ['news_form' => $news]);
+  }
+
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, News::$rules);
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      if ($request->remove == 'true') {
+          $news_form['image_path'] = null;
+      } elseif ($request->file('image')) {
+          $path = $request->file('image')->store('public/image');
+          $news_form['image_path'] = basename($path);
+      } else {
+          $news_form['image_path'] = $news->image_path;
+      }
+
+      unset($news_form['image']);
+      unset($news_form['remove']);
+      unset($news_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+      return redirect('admin/news');
+  }
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = News::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/news/');
+  }  
+}
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,48 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
+@section('title', 'ニュースの編集')
 
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>プロフィール編集画面</h1>
-    </body>
-</html>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="name" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" class="form-control" name="gender" value="{{ $profile_form->name }}">
+                            <textarea class="form-control" name="gender" rows="20">{{ $profile_form->body }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                             <textarea class="form-control" name="hobby" rows="20">{{ $profile_form->body }}</textarea>
+                        <div class="form-text text-info">
+                                設定中: {{ $profile_form->image_path }}
+                        </div>
+                        <div class="form-check">
+                    　 <label class="form-check-label">
+                          <textarea class="form-control" name="introdacution" rows="20">{{ $profile_form->body }}</textarea>
+                         </label>
+                        </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
